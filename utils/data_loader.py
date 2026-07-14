535e4ac5ad77c1b0921803914efbfb4969832ef9
Om! <omp091220@gmail.com>
2026-07-14 17:09:06 +0530

    Add files via upload

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,1 +1,25 @@
+import pandas as pd
+
+import streamlit as st
+
+@st.cache_data
+
+def load_data():
+
+    df = pd.read_csv(
+
+        "data/gtmdb.csv",
+
+        encoding="latin1",
+
+        low_memory=False
+
+    )
+
+    df["nkill"] = df["nkill"].fillna(0)
+
+    df["nwound"] = df["nwound"].fillna(0)
+
+    return df
+
 
